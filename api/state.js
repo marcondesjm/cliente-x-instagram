ba75ef0ee9724df2e4b7085850940dcfebe42882
marcondesjm <marcondes.machado.ti@gmail.com>
2026-08-15 18:04:38 -0300

    Add public bio links page

--- api/state.js
+++ api/state.js
@@ -39,7 +39,7 @@ const VERCEL_PROJECT_NAME = process.env.VERCEL_PROJECT_NAME || 'cliente-x-instag
 const ACTIVE_VERSION = {
   name: 'cliente-x-funcionando',
   label: 'Última versão funcionando',
-  appVersion: 'v4.35',
+  appVersion: 'v4.36',
   status: 'funcionando',
   stableCommit: '3314cfb',
   stableCommitUrl: 'https://github.com/marcondesjm/cliente-x-instagram/commit/3314cfb',
