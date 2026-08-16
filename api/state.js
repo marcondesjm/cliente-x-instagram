3a0577c5b594b1788ea5c216420a58d2e1328f64
marcondesjm <marcondes.machado.ti@gmail.com>
2026-08-15 21:45:48 -0300

    Add live news radar refresh button

--- api/state.js
+++ api/state.js
@@ -43,7 +43,7 @@ const VERCEL_PROJECT_NAME = process.env.VERCEL_PROJECT_NAME || 'cliente-x-instag
 const ACTIVE_VERSION = {
   name: 'cliente-x-funcionando',
   label: 'Última versão funcionando',
-  appVersion: 'v4.65',
+  appVersion: 'v4.66',
   status: 'funcionando',
   stableCommit: '3314cfb',
   stableCommitUrl: 'https://github.com/marcondesjm/cliente-x-instagram/commit/3314cfb',
